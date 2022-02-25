56d5256fcd844db65571b4491a49fe7c40de2af2
BLZXB5 <97624291+BLXB567@users.noreply.github.com>
2022-02-25 17:30:34 +0700

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -8,7 +8,7 @@ let handler = async function (m, { text, usedPrefix, command }) {
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
   age = parseInt(age)
-  if (age > 70) throw 'Umur terlalu tua'
+  if (age > 100) throw 'Bjir Kaori main hp *_*'
   if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
   user.name = name.trim()
   user.age = age
