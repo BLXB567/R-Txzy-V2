62f493b1812b89755546724d5db5b897dc00a3f9
BLZXB5 <97624291+BLXB567@users.noreply.github.com>
2022-02-25 17:35:10 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,9 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 083117436733
+Dana: 083117436733
+Saweria: http://Saweria.co/Mayumi
 `.trim()
     const button = {
         buttonText: 'List Harga',
