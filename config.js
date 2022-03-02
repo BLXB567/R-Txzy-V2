d5075c7d7f74b69162f335b34c9d4a7307392949
BLZXB5 <97624291+BLXB567@users.noreply.github.com>
2022-03-02 17:16:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285708561219', '6281331493321','6283117436733'] // Letakan nomor kamu disini
+global.owner = ['6283117436733'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
